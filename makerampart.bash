8425a24b8aa31b35538c79b4cce2bcdd9fbe9af2
earphone33 <Gruzovicekleva294@gmail.com>
2026-06-16 12:29:36 +0000

    added possibility to mount not to /tmp/ramapp

--- makerampart.bash
+++ makerampart.bash
@@ -2,8 +2,8 @@
 
 echo "makerampart.bash is now running"
 
-sudo mkdir /tmp/ramdir
-sudo mount -t tmpfs -o size=$1 tmpfs /tmp/ramdir
-echo "==> tmpfs of size $1 created"
+sudo mkdir $2
+sudo mount -t tmpfs -o size=$1 tmpfs $2
+echo "==> tmpfs of size $1 created at $2"
 
 echo "makerampart.bash exites"
