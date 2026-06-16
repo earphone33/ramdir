22bb215a8decd557d2dfbed1a6f389bb07a987ed
earphone33 <Gruzovicekleva294@gmail.com>
2026-06-16 12:49:23 +0000

    added mountpoint changes to main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 import subprocess
 import os
 
+def askYesNo(text):
+    in = input(text, "[y/n]")
+    if in.upper() == "y":
+        return True
+    return False
+
 files = subprocess.getoutput("/usr/bin/ls -A")
 files.split()
 
@@ -14,16 +20,25 @@ else:
     exit()
 
 dirToRam = input("enter a directory to save to ram (be careful!): ")
+mountpoint = input("enter a mountpoint to mount to ramdir to: ")
+
+if os.scandir(mountpoint) != "":
+    if askYesNo("directory "+mountpoint+" not empty! Remove contents?") :
+        print("removing "+mountpoint)
+        os.system("rm -r "+mountpoint)
+    else:
+        print("!Not possible to proseed! esiting!")
 
 if os.path.isdir(dirToRam):
-    print("Directory is real")
+    print("Directory "+dirToRam+" is real")
 else:
-    print("!Directory not found!")
+    print("!Directory "+dirToRam+" not found!")
     exit()
 
 sizeToCopy=str(subprocess.getoutput("du -sh "+dirToRam).split()[0])
+
 print("requiered RAM to proceed:", sizeToCopy)
-os.system("./makerampart.bash "+sizeToCopy)
+os.system("./makerampart.bash "+sizeToCopy+" "+mountpoint)
 print("creating RAM partition")
 
 print("==> Copying contents of "+dirToRam+" to /tmp/ramdir")
